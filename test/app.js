1837162b84f67a30be92624e90d730d5058d3e23
anvitasurapaneni <surapaneni.a@husky.neu.edu>
2016-11-09 15:13:14 -0500

    Update app.js

--- test/app.js
+++ test/app.js
@@ -5,7 +5,7 @@ module.exports = function(app)
     app.delete("/api/test/:id", deleteMessage);
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
-
+    ///jenkin Test
     if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
             process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
