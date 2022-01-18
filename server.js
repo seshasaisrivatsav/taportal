ec78f9c0e49c36cf8b0ee08556918013cfccdaa9
seshasaisrivatsav <seshasaisrivatsav@gmail.com>
2022-01-18 10:08:52 -0500

    fix

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 let express = require('express');
-// parses cookies for root scope. To keep the session, we need info
-// in cookies
 let cookieParser = require('cookie-parser');
 let session = require('express-session');
 let passport = require('passport'); //core passport library initialized here
 let app = express();
 
-// let connectionString = 'mongodb://127.0.0.1:27017/taportal';?
 let connectionString = 'mongodb+srv://seshasai:tris@cluster0.suhra.mongodb.net/taportal?retryWrites=true&w=majority';
+// let connectionString = 'mongodb://127.0.0.1:27017/taportal';
 let mongoose = require("mongoose");
 mongoose.connect(connectionString);
 
@@ -38,12 +36,10 @@ app.use(express.static(__dirname + '/public'));
 //let port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 let port = process.env.PORT || 3000;
-
 let project = require("./project/app.js");
 project(app);
 
 //app.listen(port, ipaddress);
-
 app.listen(port);
 
 module.exports = app;
